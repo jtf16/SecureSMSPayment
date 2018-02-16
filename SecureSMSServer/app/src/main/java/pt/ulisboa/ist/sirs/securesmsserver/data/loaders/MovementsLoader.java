bd1d5f464d66e25d16ebe695da965613b8f04b50
facas <joaotavaresfreitas@hotmail.com>
2018-02-16 03:56:39 +0000

    1.9 SecureSMSServer: Create loaders with AsyncTaskLoaders to retrieve values from database

---
+++ SecureSMSServer/app/src/main/java/pt/ulisboa/ist/sirs/securesmsserver/data/loaders/MovementsLoader.java
@@ -0,0 +1,159 @@
+package pt.ulisboa.ist.sirs.securesmsserver.data.loaders;
+
+import android.content.Context;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.os.OperationCanceledException;
+
+import java.util.List;
+
+import pt.ulisboa.ist.sirs.securesmsserver.InterestingConfigChanges;
+import pt.ulisboa.ist.sirs.securesmsserver.data.AppDatabase;
+import pt.ulisboa.ist.sirs.securesmsserver.data.objects.main.Movement;
+
+public class MovementsLoader extends
+        AsyncTaskLoader<List<Movement>> {
+
+    private List<Movement> mData;
+
+    final InterestingConfigChanges mLastConfig = new InterestingConfigChanges();
+
+    private AppDatabase appDatabase;
+
+    public MovementsLoader(Context context) {
+        super(context);
+        appDatabase = AppDatabase.getAppDatabase(context);
+    }
+
+    /**
+     * Called on a worker thread to perform the actual load and to return
+     * the result of the load operation.
+     * <p>
+     * Implementations should not deliver the result directly, but should return them
+     * from this method, which will eventually end up calling {@link #deliverResult} on
+     * the UI thread.  If implementations need to process the results on the UI thread
+     * they may override {@link #deliverResult} and do so there.
+     * <p>
+     * To support cancellation, this method should periodically check the value of
+     * {@link #isLoadInBackgroundCanceled} and terminate when it returns true.
+     * Subclasses may also override {@link #cancelLoadInBackground} to interrupt the load
+     * directly instead of polling {@link #isLoadInBackgroundCanceled}.
+     * <p>
+     * When the load is canceled, this method may either return normally or throw
+     * {@link OperationCanceledException}.  In either case, the {@link Loader} will
+     * call {@link #onCanceled} to perform post-cancellation cleanup and to dispose of the
+     * result object, if any.
+     *
+     * @return The result of the load operation.
+     * @throws OperationCanceledException if the load is canceled during execution.
+     * @see #isLoadInBackgroundCanceled
+     * @see #cancelLoadInBackground
+     * @see #onCanceled
+     */
+    @Override
+    public List<Movement> loadInBackground() {
+        // Retrieve all known movements.
+        return appDatabase.movementDao().loadAllMovements();
+    }
+
+    /**
+     * Called when there is new data to deliver to the client. The
+     * super class will take care of delivering it; the implementation
+     * here just adds a little more logic.
+     */
+    @Override
+    public void deliverResult(List<Movement> data) {
+        if (isReset()) {
+            // An async query came in while the loader is stopped.  We
+            // don't need the result.
+            if (data != null) {
+                onReleaseResources(data);
+            }
+        }
+        List<Movement> oldData = mData;
+        mData = data;
+
+        if (isStarted()) {
+            // If the Loader is currently started, we can immediately
+            // deliver its results.
+            super.deliverResult(data);
+        }
+
+        // At this point we can release the resources associated with
+        // 'oldData' if needed; now that the new result is delivered we
+        // know that it is no longer in use.
+        if (oldData != null) {
+            onReleaseResources(oldData);
+        }
+    }
+
+    /**
+     * This method is invoked by the LoaderManager whenever the loader is started
+     */
+    @Override
+    protected void onStartLoading() {
+        if (mData != null) {
+            // If we currently have a result available, deliver it
+            // immediately.
+            deliverResult(mData);
+        }
+
+        // Has something interesting in the configuration changed since we
+        // last built the app list?
+        boolean configChange = mLastConfig.applyNewConfig(getContext().getResources());
+
+        if (takeContentChanged() || mData == null || configChange) {
+            // If the data has changed since the last time it was loaded
+            // or is not currently available, start a load.
+            forceLoad();
+        }
+    }
+
+    /**
+     * Handles a request to stop the Loader.
+     */
+    @Override
+    protected void onStopLoading() {
+        // Attempt to cancel the current load task if possible.
+        cancelLoad();
+    }
+
+    /**
+     * Handles a request to cancel a load.
+     */
+    @Override
+    public void onCanceled(List<Movement> data) {
+        super.onCanceled(data);
+
+        // At this point we can release the resources associated with 'data'
+        // if needed.
+        onReleaseResources(data);
+    }
+
+    /**
+     * Handles a request to completely reset the Loader.
+     */
+    @Override
+    protected void onReset() {
+        super.onReset();
+
+        // Ensure the loader is stopped
+        onStopLoading();
+
+        // At this point we can release the resources associated with 'data'
+        // if needed.
+        if (mData != null) {
+            onReleaseResources(mData);
+            mData = null;
+        }
+    }
+
+    /**
+     * Helper function to take care of releasing resources associated
+     * with an actively loaded data set.
+     */
+    protected void onReleaseResources(List<Movement> data) {
+        // For a simple List<> there is nothing to do.  For something
+        // like a Cursor, we would close it here.
+    }
+}
