9bc07b3a4badf5c9236aac0a60e73dc3f9372b3f
facas <joaotavaresfreitas@hotmail.com>
2018-02-28 13:36:56 +0000

    2.4 SecureSMSClient: Make data objects, entities of database

--- SecureSMSClient/app/src/main/java/pt/ulisboa/ist/sirs/securesmsclient/data/objects/Client.java
+++ SecureSMSClient/app/src/main/java/pt/ulisboa/ist/sirs/securesmsclient/data/objects/Client.java
@@ -1,14 +1,36 @@
 package pt.ulisboa.ist.sirs.securesmsclient.data.objects;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+@Entity(tableName = "Client",
+        indices = {@Index(value = {"IBAN"}, unique = true)})
 public class Client {
 
+    @PrimaryKey
+    @ColumnInfo(name = "id")
+    private int uid;
+
+    @ColumnInfo(name = "IBAN")
     private String IBAN;
+
+    @ColumnInfo(name = "balance")
     private float balance;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
 
     public String getIBAN() {
         return this.IBAN;
--- SecureSMSClient/app/src/main/java/pt/ulisboa/ist/sirs/securesmsclient/data/objects/Movement.java
+++ SecureSMSClient/app/src/main/java/pt/ulisboa/ist/sirs/securesmsclient/data/objects/Movement.java
@@ -1,14 +1,31 @@
 package pt.ulisboa.ist.sirs.securesmsclient.data.objects;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
 import java.util.Date;
 
+@Entity(tableName = "Movement")
 public class Movement {
 
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
     private int uid;
+
+    @ColumnInfo(name = "from_or_to")
     private int fromOrTo;
+
+    @ColumnInfo(name = "IBAN")
     private String IBAN;
+
+    @ColumnInfo(name = "amount")
     private float amount;
+
+    @ColumnInfo(name = "date")
     private Date date;
+
+    @ColumnInfo(name = "state")
     private String state;
 
     public int getUid() {
